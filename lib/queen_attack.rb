f4cc58fef398d33b5b4a91205d167ee62d8a705c
Jeff Seymour, Kyle Langley and Krystan Menne <kyle@>
2016-01-04 16:06:37 -0800

    Returns true if coordinates are diagonal

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,6 +1,6 @@
 class Array
   define_method(:queen_attack?) do |enemy_position|
-    self[0] == enemy_position[0] || self[1] == enemy_position[1]
-
+    self[0] == enemy_position[0] || self[1] == enemy_position[1] ||
+    (self[0] - enemy_position[0]).abs == (self[1] - enemy_position[1]).abs
   end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -5,10 +5,20 @@ describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
     expect([1,1].queen_attack?([2,3])).to(eq(false))
   end
+
   it('is true if the coordinates are horizontal') do
     expect([1,1].queen_attack?([1,8])).to(eq(true))
   end
+
   it('is true if the coordinates are vertical') do
     expect([1,1].queen_attack?([8,1])).to(eq(true))
+  end
+
+  it('is true if the coordinates are diagonal') do
+    expect([1,1].queen_attack?([8,8])).to(eq(true))
+  end
+
+  it('is true if the coordinates are diagonal') do
+    expect([3,3].queen_attack?([1,5])).to(eq(true))
   end
 end
